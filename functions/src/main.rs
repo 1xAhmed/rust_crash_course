23dbef6d96e09f14a39c2de8081e36c67eee377a
1xahmed <1xahmedali@gmail.com>
2024-04-09 20:09:24 +0500

    functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let x = do_stuff(2.0, 12.5);
+}
+
+
+fn do_stuff(qty: f64, oz: f64) -> f64 {
+    // return qty * oz;   this is okay, but im idiomatic rust, shorter way is preferred
+
+    qty * oz    // leave last expression of the block, then it will return as value of the block.
 }
