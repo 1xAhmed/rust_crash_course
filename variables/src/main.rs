bc8427b06a9e6f88b47f0fc88b4ca15eecf5eaa7
1xahmed <1xahmedali@gmail.com>
2024-04-09 14:24:32 +0500

    Variable sample project | added target folder in .gitignore

---
+++ variables/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // without type annotation
+    let bunnies = 2;
+
+    // with type annotation
+    let sunny: i32 = 4;
+
+    // initalize multiple variables at once
+    let (apple, banana) = (8, 5);
+
+    
+}
