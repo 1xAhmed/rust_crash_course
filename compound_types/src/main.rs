9db85ff84d4263e43831af7eaf552441a96e3539
1xahmed <1xahmedali@gmail.com>
2024-04-10 05:13:21 +0500

    compound types completed

--- compound_types/src/main.rs
+++ compound_types/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     let (jet, fuels, ammos) = infor;
 
 
-    // Array, by contrast, store multple values of same type. You can specify them litrally with [] and commas or with a value on how many you want seperated by semi-colon
+    // Array, by contrast, store multple values of same type. You can specify them litrally with [] and commas or with a value on how many you want seperated by semi-colon. NOte that type annotation for array always uses semi-colon form, even when you specify all the literal values in array. Array's are limited to size 32 above which they loses most of their functionality. Arrays live on the stack by default. SO you will usally use `vec` or slices of vectors, instead of arrays
     let buf = [1, 2, 3];
     let buff = [0; 3];  // [value; howmany]
 
