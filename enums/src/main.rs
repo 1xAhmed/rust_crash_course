a3d05cae37d32102052261fc0a63fa191fb10df0
1xahmed <1xahmedali@gmail.com>
2024-04-12 15:43:29 +0500

    enums in rust

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -89,6 +89,52 @@ fn main() {
 
 
     // Let's look a little more option. Here's the definition of option again. Option is used whenever something might be absent here is how you could create a non variant of an option. I specified the type that some will wrap an angle brackets after the option notice that I don't have a use statement bringing into scope option or its variants sum or none from the standard Library since option and its variants are used so much. They're already included in the standard Prelude, which is the list of items from the standard library that are always brought into scope by default.
-    let mut x: Option<i32> = None
+    let mut x = None::<i32>;
 
+
+    // If you ever use option with a concrete type, then the compiler will infer the type which means you can leave type annotation off of the declaration most of the time
+    let mut y = None;
+    y = Some(5);
+
+    // There's a handy helper method called `is_some` that returns true of y as is_some variant
+    y.is_some();    // true
+    // There is also is none method that does just the opposed. 
+    y.is_none();    // false
+
+    // Option implements the into iterator trait, so you can also treat it similar to a vector of 0 or 1 items and put it in a for loop. You ought to read through the methods for option because you will end up using them a lot
+    for i in y {
+        println!("{}", i);      // prints 5
+    }
+
+
+    // The other important enum is `result`, result will use whenever something might have a useful result or might have an error. this turns up especially often in the io module.
+    // Here is the definition of the result enum, you'll see that the type wrapped by `Ok`, and the type wrapped by `Err` are both generic but independent of each other second the must use annotation makes it a compiler warning to silently drop a result. You have to do something with it, rust strongly encourages you to look at all possible errors and make a conscious choice what to do with each one.
+    // Anytime you deal with IO failure is a possibility, so result are used alot there
+    #[must_use]
+    enum Result<T, E> {
+        Ok(T),
+        Err(E),
+    }
+
+    // Lets see it in action. Here I bring standard file system into the scope, then try to open a file, this returns a result, because the file might not open successfully. Since I dropped the result without doing anything with it. Compiler will give me warning that result must be handled.
+    use std::fs::File;
+    File::open("foo");
+
+    // Lets do something with our result. The simplest thing you could choose to unwrap the result with the unwrapped method. If the result is an `Ok`, then this gives you the file struct that you wanted. If the result is an `Err` then this crashes the program
+    let res = File::open("foo");
+    let f = res.unwrap();
+
+    // Another option is the expect method, its exactly the same as `unwrap` except that the string that you pass to expect is also printed in the crash output, so you can provide custom context as to why the crash occured.
+    let fi =  res.expect("error message");
+
+    // Just like option, there are helper methods like `is_ok` and is error that returns booleans. Here we know unwrap will never crash because we made sure that it was an okay already
+    if res.is_ok() {
+        let f = res.unwrap();
+    }
+
+    // You can always do full pattern matching as well. Here I match on a result and execute different blocks depending on what O got back.
+    match res {
+        Ok(f) => { /* do stuff */ },
+        Err(e) => { /* do stuff */ },
+    }
 }
